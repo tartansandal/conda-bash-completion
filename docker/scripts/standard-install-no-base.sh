51c87466c6bcbe700f8ba74ebddcd1b523f56593
Kahlil Hodgson <tartansandal@users.noreply.github.com>
2022-04-27 15:50:49 +1000

    add no-base testing scripts

---
+++ docker/scripts/standard-install-no-base.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -xe
+    
+
+
+# Add a test user under debian/ubuntu (remove any existing test user first)
+getent passwd test-cbc && userdel --remove test-cbc
+adduser test-cbc --disabled-password --gecos "Testing CBC"
+
+
+conda=./miniconda3/bin/conda
+
+sudo --user test-cbc --login -- <<EOF
+# Echo commands and abort on errors
+set -xe
+
+# Download and install miniconda
+bash /Miniconda3-latest-Linux-x86_64.sh -b
+
+# Setup conda with defaults
+$conda init
+$conda config --set auto_activate_base False
+
+# Install conda-bash-completion
+$conda install -c conda-forge conda-bash-completion
+
+EOF
+
+# Get a new shell with the above enviroment
+su -l test-cbc
---
+++ docker/scripts/xdg-install-no-base.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -xe
+    
+# Add a test user under debian/ubuntu (remove any existing test user first)
+getent passwd test-cbc && userdel --remove test-cbc
+adduser test-cbc --disabled-password --gecos "Testing CBC"
+
+conda=./miniconda3/bin/conda
+
+sudo --user test-cbc --login -- <<EOF
+# Echo commands and abort on errors
+set -xe
+
+# Install miniconda
+bash /Miniconda3-latest-Linux-x86_64.sh -b
+
+# Setup conda without default activation
+$conda init
+$conda config --set auto_activate_base False
+
+# Install conda-bash-completion into the XDG directory outside of miniconda3 
+mkdir -p .local/share/bash-completion/completions
+pushd .local/share/bash-completion/completions || exit
+curl -sO https://raw.githubusercontent.com/tartansandal/conda-bash-completion/master/conda
+popd || exit
+
+EOF
+
+# Get a new shell with the above enviroment
+su -l test-cbc
